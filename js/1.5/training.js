dbbf817c8e7f75d252324759ae281bc1ba6f5a4c
Nikolai Krivetski <nick.krivetskiy@gmail.com>
2016-06-08 01:27:42 +0300

    Added 1.5 task

--- js/1.5/training.js
+++ js/1.5/training.js
@@ -1,5 +1,63 @@
 var module = angular.module('myApp', []);
 
-module.controller("SimpleController", ["$scope", function($scope) {
- 
+module.controller("SimpleController", ["$scope", function ($scope) {
+
+}]);
+
+module.directive("modalWindow", function () {
+
+    return {
+        scope: {
+            modalWindowTitle: "<title",
+            modalWindowActionHandler: "&actionCallback"
+        },
+        restrict: "E",
+        transclude: true,
+        link: function(scope, iElement, iAttrs, controller) {
+            
+        },
+        controller: function($scope, $element, $attrs, $transclude){
+            $scope.innerModalWindowActionHandler = function($event) {
+                $scope.modalWindowActionHandler({event: $event, sender: $event.target});
+                return false;
+            }
+        },
+        template: 
+"<!-- Button trigger modal -->" +
+"<button type='button' class='btn btn-primary btn-lg' data-toggle='modal' data-target='#myModal'>" +
+"Click Me" + 
+"</button>" +
+'<!-- Modal -->'+
+'<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">'+
+  '<div class="modal-dialog" role="document">'+
+    '<div class="modal-content">'+
+      '<div class="modal-header">'+
+        '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+        '<h4 class="modal-title" id="myModalLabel">{{ modalWindowTitle }}</h4>'+
+      '</div>'+
+      '<div class="modal-body">'+
+      '<ng-transclude />' +
+      '</div>'+
+      '<div class="modal-footer">'+
+        '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>'+
+        '<button type="button" class="btn btn-primary" ng-click="modalWindowActionHandler({event: $event })">OK</button>'+
+      '</div>'+
+    '</div>'+
+  '</div>'+
+'</div>' 
+    };
+
+});
+
+module.run(["$rootScope", function ($rootScope) {
+    $rootScope.doSomeMagic = function () {
+        console.log("Some magic to console");
+    };
+    
+    $rootScope.modalWindowTitle = "Some Window Title";
+    
+    $rootScope.modalWindowActionHandler = function(event, sender) {
+        console.log("Modal Window Action Handler", event, sender);
+        return false;
+    }
 }]);
--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 
 #### This repository contains code for angular training..
 
-#### - [Task - 1]
+#### [Task - 1]
  To display the inheritance of scopes in angularjs applications do the following:
  - with ng-init create a shared value in root scope
  - create parentCtrl1 and parentCtrl2 on the same level under root scope
@@ -12,11 +12,12 @@
  - in the scope level of ChildCtrl1 and ChildCtrl2 place inputs with ng-model bound to shared value
  - near each inputs place outputs of shared value with {{}} expression
  - improve the application so that all the {{}} show the same values after different scenarios of typing in valuse into different inputs.
-#### - [Task - 1.5]
+
+#### [Task - 1.5]
  - Implement modal-window directive that will take title and actionCallback as params (title is string for window title, actionCallback to handle OK-button click). 
 You should use ng-transclude directive so that you could insert any content into modal window. Content is context-dependent. Modal-window dorective use example:
 ```html
-<modal-window title="modalWindowTitle" action-callback="modalWindowActionHandler(event, sender)">
+<modal-window title="modalWindowTitle" action-callback="modalWindowActionHandler">
 	<p>
 		Although, in a way, the Congressional powers in Article 9 made the "league of states as cohesive and strong as any similar sort of republican confederation in history",
 		<sup id="cite_ref-11" class="reference">
